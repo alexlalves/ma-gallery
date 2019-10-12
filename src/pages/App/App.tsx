63f6cace42fa8b345833d6f3adc77b59b5727324
Magmagan <alexlalves@live.com>
2019-10-12 15:50:43 -0300

    mock image view works when app window is large enough

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from '../../assets/logo/logo512.png';
+import _furryImage from '../../assets/sample_images/furry.jpg';
+import _hentaiImage from '../../assets/sample_images/hentai.jpg';
+import _transImage from '../../assets/sample_images/transgender.jpg';
+import _typhlosionImage from '../../assets/sample_images/typhlosion.jpg';
+import _typhlosionImageTwo from '../../assets/sample_images/typhlosion_2.png';
 import Toolbar from '../../components/Toolbar/Toolbar';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className='App'>
-      <Toolbar />
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload..
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App__toolbar'>
+        <Toolbar/>
+      </div>
+      <section className='App__images'>
+        <img
+          alt='logo bh'
+          src={_typhlosionImageTwo}
+          className='App__images__image App__images__image--background'
+        />
+        <img
+          alt='logo a'
+          className='App__images__image App__images__image--main'
+          src={_typhlosionImageTwo}
+        />
+      </section>
     </div>
   );
 }
