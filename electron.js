c21a784d2475a581a5ca8ba8bc692538e0ceb53c
Magmagan <alexlalves@live.com>
2019-10-27 02:37:34 -0300

    lint electron.js

--- electron.js
+++ electron.js
@@ -2,9 +2,8 @@ const electron = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
 const fs = require('fs');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const ipcMain = electron.ipcMain;
+
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 let fileName;
@@ -19,17 +18,17 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-    }
+    },
   });
 
-  const url = isDev? 'http://localhost:3000': `file://${path.join(__dirname, '../build/index.html')}`
+  const url = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
   mainWindow.loadURL(url);
-  mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on('closed', () => { mainWindow = null; });
 }
 
 app.on('ready', createWindow);
-app.on('window-all-closed', () => {if (process.platform !== 'darwin') {app.quit();}});
-app.on('activate', () => {if (mainWindow === null) {createWindow();}});
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') { app.quit(); } });
+app.on('activate', () => { if (mainWindow === null) { createWindow(); } });
 
 ipcMain.on('opened-file-request', (event) => {
   fileName = isDev ? '/sample_images/gif.gif' : process.argv[1];
@@ -50,4 +49,4 @@ ipcMain.on('opened-file-directory', (event) => {
 
     event.returnValue = files;
   }
-})
+});
