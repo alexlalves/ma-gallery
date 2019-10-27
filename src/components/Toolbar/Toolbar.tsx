55701e8b1bf687003ef727bd348214e379197f40
Magmagan <alexlalves@live.com>
2019-10-27 13:13:32 -0300

    position filename in center of toolbar

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -13,15 +13,15 @@ class Toolbar extends React.PureComponent<IProps> {
     return (
       <nav className='Toolbar'>
         <div className='Toolbar__branding'>
-          <img className='Toolbar__branding__logo' src={logo} alt='aaa' />
+          <img className='Toolbar__branding__logo' src={logo} alt='' />
           <span className='Toolbar__branding__text'>
             Ma Gallery
           </span>
         </div>
-        <div>
+        <div className='Toolbar__filename'>
           { props.filename }
         </div>
-        <div>
+        <div className='Toolbar__menu-icon'>
           | | |
         </div>
       </nav>
