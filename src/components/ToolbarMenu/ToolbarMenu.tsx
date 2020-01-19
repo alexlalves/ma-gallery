075ff9f68beb43a2f0d0d565545b1312fdc7aed8
Magmagan <magmagan@live.com>
2020-01-19 19:58:53 -0300

    fullscreen button now sets window to fullscreen

--- src/components/ToolbarMenu/ToolbarMenu.tsx
+++ src/components/ToolbarMenu/ToolbarMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './ToolbarMenu.css';
 import AboutIcon from '../../assets/icons/about_light.svg';
 import FullscreenIcon from '../../assets/icons/fullscreen_light.svg';
@@ -6,7 +7,16 @@ import OpenfileIcon from '../../assets/icons/openfile_light.svg';
 import SettingsIcon from '../../assets/icons/settings_light.svg';
 import SlideshowIcon from '../../assets/icons/slideshow_light.svg';
 
+const electron = window.require('electron');
+
 class ToolbarMenu extends React.PureComponent<{}> {
+  public setFullscreen = () => {
+    const window = electron.remote.getCurrentWindow();
+    const isFullscreen = window.isFullScreen();
+
+    window.setFullScreen(!isFullscreen);
+  }
+
   public render() {
     return (
       <aside className='ToolbarMenu'>
@@ -17,10 +27,14 @@ class ToolbarMenu extends React.PureComponent<{}> {
               src={OpenfileIcon}
             />
           </li>
-          <li className='ToolbarMenu__options__option'>
+          <li
+            className='ToolbarMenu__options__option'
+            onClick={this.setFullscreen}
+          >
             <img
               className='ToolbarMenu__options__option__image'
               src={FullscreenIcon}
+              alt='Full screen'
             />
           </li>
           <li className='ToolbarMenu__options__option'>
