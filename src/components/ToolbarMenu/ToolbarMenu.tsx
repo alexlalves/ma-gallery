b528ee18bc1ea5e320c67cfe4597ff4de5cc3727
Magmagan <magmagan@live.com>
2020-01-19 19:39:07 -0300

    add elements to toolbar menu

--- src/components/ToolbarMenu/ToolbarMenu.tsx
+++ src/components/ToolbarMenu/ToolbarMenu.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import './ToolbarMenu.css';
+import AboutIcon from '../../assets/icons/about_light.svg';
+import FullscreenIcon from '../../assets/icons/fullscreen_light.svg';
+import OpenfileIcon from '../../assets/icons/openfile_light.svg';
+import SettingsIcon from '../../assets/icons/settings_light.svg';
+import SlideshowIcon from '../../assets/icons/slideshow_light.svg';
 
 class ToolbarMenu extends React.PureComponent<{}> {
   public render() {
     return (
       <aside className='ToolbarMenu'>
-        <ul>
-          <li>
-            Open File
+        <ul className='ToolbarMenu__options'>
+          <li className='ToolbarMenu__options__option'>
+            <img
+              className='ToolbarMenu__options__option__image'
+              src={OpenfileIcon}
+            />
           </li>
-          <li>
-            Slideshow
+          <li className='ToolbarMenu__options__option'>
+            <img
+              className='ToolbarMenu__options__option__image'
+              src={FullscreenIcon}
+            />
           </li>
-          <li>
-            Fullscreen
+          <li className='ToolbarMenu__options__option'>
+            <img
+              className='ToolbarMenu__options__option__image'
+              src={SlideshowIcon}
+            />
           </li>
-          <li>
-            Settings
+          <li className='ToolbarMenu__options__option'>
+            <img
+              className='ToolbarMenu__options__option__image'
+              src={SettingsIcon}
+            />
           </li>
-          <li>
-            About
+          <li className='ToolbarMenu__options__option'>
+            <img
+              className='ToolbarMenu__options__option__image'
+              src={AboutIcon}
+            />
           </li>
         </ul>
       </aside>
